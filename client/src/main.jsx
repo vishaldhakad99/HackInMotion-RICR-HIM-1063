eb24389de7e8eb3becc6e0361754632ed290c100
HodManagmentSystem <harshubankhede@gmail.com>
2026-08-14 18:16:49 +0530

    Enhance CityMap with city search functionality and update MapView for searched location display

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'leaflet/dist/leaflet.css'
 import './index.css'
 import App from './App.jsx'
 
@@ -8,4 +9,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
 
